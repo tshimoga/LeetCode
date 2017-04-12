08da1a43f55e52a99026bef325af5017707b4ef6
Tarun Shimoga <tarun201987@gmail.com>
2017-04-11 22:27:14 -0700

    Update MagicalString.java

--- MagicalString.java
+++ MagicalString.java
@@ -1,9 +1,15 @@
+/*
+Question:
+A magical string S consists of only '1' and '2' and obeys the following rules:
+The string S is magical because concatenating the number of contiguous occurrences of characters '1' and '2' generates the string S itself.
+The first few elements of string S is the following: S = "1221121221221121122……"
+*/
+
 public class MagicalString {
     public int magicalString(int n) {
         if(n==0) {
             return 0;
-        }
-        
+        } 
         int count=1;
         StringBuilder str = new StringBuilder("122");
         boolean one = true;
